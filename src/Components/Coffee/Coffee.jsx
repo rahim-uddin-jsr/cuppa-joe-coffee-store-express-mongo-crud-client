0f5902e0e241553d85f96cb16789920bb1b1a7b3
dev-rahim <rahimuddin5915@gmail.com>
2023-05-09 21:43:48 +0600

    coffee delete from api done

--- src/Components/Coffee/Coffee.jsx
+++ src/Components/Coffee/Coffee.jsx
@@ -1,9 +1,38 @@
 /* eslint-disable react/prop-types */
 import { FaEye, FaPencilAlt, FaTrashAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
 const Coffee = ({ coffee }) => {
   const { name, chef, supplier, photo, _id } = coffee;
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:5000/delete/${id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.deletedCount > 0) {
+              Swal.fire("Deleted!", "Your Coffee has been deleted.", "success");
+            }
+          });
+      } else if (
+        /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+      ) {
+        Swal.fire("Cancelled", "Your imaginary file is safe :)", "error");
+      }
+    });
+  };
   return (
     <div className="card card-side bg-base-100 shadow-xl justify-evenly ">
       <figure>
@@ -24,7 +53,10 @@ const Coffee = ({ coffee }) => {
               <FaPencilAlt></FaPencilAlt>
             </button>
           </Link>
-          <button className="btn  btn-circle btn-sm">
+          <button
+            onClick={() => handleDelete(_id)}
+            className="btn  btn-circle btn-sm"
+          >
             <FaTrashAlt />
           </button>
         </div>
